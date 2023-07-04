702e3696f5c09425c6f4230b9078ca516b9e4fce
msilis <mikssilis@gmail.com>
2023-07-04 06:09:42 +0100

    moved inded to api folder

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 8086;
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+//MongoDB
+//Connect
+mongoose.connect(process.env.DATABASE_URL, { dbName: "sillystories" });
+
+//DB Connection Info
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Silly Stories Database"));
+
+const groupRouter = require("./routes/sillyRoutes");
+
+//Cors options
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+};
+
+app.use(cors(corsOptions));
+app.use("/", groupRouter);
+app.use(bodyParser.json());
+
+app.listen(port, () => console.log(`Listening on ${port}`));
