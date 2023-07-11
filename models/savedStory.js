672a62657eab64029a91d6066af6dcbcf5816902
msilis <mikssilis@gmail.com>
2023-07-11 20:37:02 +0100

    save story route

---
+++ models/savedStory.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const savedSchema = new mongoose.Schema(
+  {
+    story: {
+      type: String,
+      required: true,
+    },
+  },
+  { collection: "savedStories" }
+);
+
+module.exports = mongoose.model("SavedStory", savedSchema);
--- routes/sillyRoutes.js
+++ routes/sillyRoutes.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Story = require("../models/story");
+const savedStory = require("../models/savedStory");
 
 router.get("/", (req, res) => {
   res.status(200).send("Silly Stories Back End");
@@ -29,6 +30,21 @@ router.get("/randomStory", async (req, res) => {
     res.status(500).send({
       msg: "There was an error with the server. I could not get a random story.",
     });
+  }
+});
+
+//Save story
+router.post("/saveStory", async (req, res) => {
+  try {
+    const saveStory = new savedStory({
+      story: req.body.story,
+    });
+
+    const addSavedStory = await saveStory.save();
+    res.status(201).json(addSavedStory);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({ msg: "There was an error saving your story!" });
   }
 });
 
